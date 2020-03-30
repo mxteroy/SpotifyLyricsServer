5705581a83047a7bf72e27dfff3c5af194547533
James <mxteroy@gmail.com>
2020-03-29 21:59:53 -0500

    first commit

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import request
+from bs4 import BeautifulSoup
+from auth import base_url, headers
+
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_song_path():
+    search_url = base_url + "/search"
+    artist = request.args.get('artist')
+    song = request.args.get('song')
+    data = {'q': song}
+    response = requests.get(search_url, data=data, headers=headers)
+    json = response.json()
+    song_info = None
+    for hit in json["response"]["hits"]:
+        if hit["result"]["primary_artist"]["name"].lower() == artist.lower():
+            song_info = hit
+            break
+
+    if song_info:
+        api_path = song_info["result"]["api_path"]
+        return get_song_lyrics(api_path)
+        
+def get_song_lyrics(api_path):
+    song_url = base_url + api_path
+    response = requests.get(song_url, headers=headers)
+    json = response.json()
+    path = json["response"]["song"]["path"]
+
+    page_url = "http://genius.com" + path
+    page = requests.get(page_url)
+    html = BeautifulSoup(page.text, "html.parser")
+    [h.extract() for h in html('script')]
+    lyrics = html.find("div", class_="lyrics").get_text()
+    return lyrics
