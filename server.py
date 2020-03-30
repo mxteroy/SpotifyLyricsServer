4301450b4cd6dc9dc6f9bb56011d6c3dd26029eb
James <mxteroy@gmail.com>
2020-03-30 02:20:54 -0500

    allowed cross origin

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask import request
 from bs4 import BeautifulSoup
 from auth import base_url, headers
+from flask_cors import CORS
 
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def get_song_path():
