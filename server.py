dd5671d180b485c1a6b755e332504352c9932354
James <mxteroy@gmail.com>
2020-03-29 22:22:19 -0500

    port = 5000

--- server.py
+++ server.py
@@ -39,4 +39,4 @@ def get_song_lyrics(api_path):
     return lyrics
 
 if __name__ == '__main__':
-    app.run(debug=False, host="0.0.0.0", port=80)
+    app.run(debug=False, host="0.0.0.0", port=5000)
