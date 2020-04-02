dc9b4bda72c8fae74adce80df7c13ff482c51ba8
James <mxteroy@gmail.com>
2020-04-02 18:05:56 -0500

    extract text from a tags

--- server.py
+++ server.py
@@ -37,6 +37,7 @@ def get_song_lyrics(api_path):
     page = requests.get(page_url)
     html = BeautifulSoup(page.text, "html.parser")
     [h.extract() for h in html('script')]
+    [a.text for a in html('a', 'referent')]
     lyrics = html.find("div", class_="lyrics").decode_contents()
     return lyrics
 
